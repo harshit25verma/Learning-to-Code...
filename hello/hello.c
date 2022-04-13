670d688798b0231052fd31a126a79edf470c762d
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-13 21:44:35 +0000

    Wed, Apr 13, 2022, 9:44 PM +00:00

--- hello/hello.c
+++ hello/hello.c
@@ -3,6 +3,6 @@
 
  int main(void)
  {
-     string name = get_string("What's your name? ");
-     printf("Hello, %s\n", name);
+     string name = get_string("What's your name? "); //To get answer from the User
+     printf("Hello, %s\n", name); //To provide answer as well as the text Hello
  }
