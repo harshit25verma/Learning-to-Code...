5175384a94bafa490aad1f73329bb89f52be4e15
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-14 19:49:43 +0000

    Thu, Apr 14, 2022, 7:49 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -3,12 +3,14 @@
 
 int main(void)
 {
+    //We need the self identifying loop
   int h;
   do
   {
      h = get_int("Height: ");
   }
   while(h>8 || h<1);
+  //Now we try to make the blocks code
   for(int i = 0; i < h; i++)
   {
       for(int j = 0; j < h; j++)
