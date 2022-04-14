10c1511c6d711e1c9fe17c7f161268789de2716e
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-14 22:38:33 +0000

    Thu, Apr 14, 2022, 10:38 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -14,6 +14,10 @@ int main(void)
   int i;
   for(i=1 ; i <= h ; i++)
   {
+    for(int d = 1; d < h ;  )
+    {
+      printf(".");
+    }
       for(int j = 1; j <= i; j++)
       {
      printf("_");
