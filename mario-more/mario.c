508f17339d930188fd1e24fecabe13459877a0b2
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-15 19:28:35 +0000

    Fri, Apr 15, 2022, 7:28 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -11,7 +11,8 @@ int main(void)
     }
     while ( h > 8 || h < 1);
     //Now we try to make the blocks code
-    int i; int s=h;
+    int i;
+    int s=h;
     for ( i = 1 ; i <= h ; i++ && s--)
     {
         for (int d = 1; d < s ; d++)
