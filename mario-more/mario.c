8aa7cd458d8a88aad0279b7f68d02470dc8dd5b4
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-15 19:23:46 +0000

    Fri, Apr 15, 2022, 7:23 PM +00:00

--- mario-more/mario.c
+++ mario-more/mario.c
@@ -5,29 +5,28 @@ int main(void)
 {
     //We need the self identifying loop
   int h;
-  do
-  {
-     h = get_int("Height: ");
-  }
-  while(h>8 || h<1);
-  //Now we try to make the blocks code
-  int i;
-  int s=h;
-  for (i=1 ; i <= h ; i++ && s--)
-  {
-    for (int d = 1; d < s ; d++)
-    {
-      printf(" ");
+   do
+   {
+       h = get_int("Height: ");
+   }
+   while(h>8 || h<1);
+   //Now we try to make the blocks code
+ int i; int s=h;
+   for (i=1 ; i <= h ; i++ && s--)
+   {
+       for (int d = 1; d < s ; d++)
+       {
+           printf(" ");
+       }
+       for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("  ");
+        for (int j = 1; j <= i; j++)
+        {
+            printf("#");
+        }
+        printf("\n");
     }
-      for (int j = 1; j <= i; j++)
-      {
-         printf("#");
-      }
-      printf("  ");
-      for (int j = 1; j <= i; j++)
-      {
-     printf("#");
-      }
-    printf("\n");
-  }
 }
