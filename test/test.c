41564eb4b1ca0c1404a1d46021bab1ca17c0917f
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-04-15 12:58:23 +0000

    Fri, Apr 15, 2022, 12:58 PM +00:00

--- test/test.c
+++ test/test.c
@@ -1,19 +1,69 @@
-// Return value
-
 #include <cs50.h>
 #include <stdio.h>
+#include <math.h>
 
-float discount(float price);
+int get_cents(void);
+int calculate_quarters(int cents);
+int calculate_dimes(int cents);
+int calculate_nickels(int cents);
+int calculate_pennies(int cents);
 
 int main(void)
 {
-    float regular = get_float("Regular Price: ");
-    float sale = discount(regular);
-    printf("Sale Price: %.2f\n", sale);
+    // Ask how many cents the customer is owed
+    do
+    int cents = get_cents();
+    while(cents)
+
+    // Calculate the number of quarters to give the customer
+    int quarters = calculate_quarters(cents);
+    cents = cents - quarters * 25;
+
+    // Calculate the number of dimes to give the customer
+    int dimes = calculate_dimes(cents);
+    cents = cents - dimes * 10;
+
+    // Calculate the number of nickels to give the customer
+    int nickels = calculate_nickels(cents);
+    cents = cents - nickels * 5;
+
+    // Calculate the number of pennies to give the customer
+    int pennies = calculate_pennies(cents);
+    cents = cents - pennies * 1;
+
+    // Sum coins
+    int coins = quarters + dimes + nickels + pennies;
+
+    // Print total number of coins to give the customer
+    printf("%i\n", coins);
 }
 
-// Discount price
-float discount(float price)
+int get_cents(void)
 {
-    return price * .85;
+    // TODO
+    return 0;
+}
+
+int calculate_quarters(int cents)
+{
+    // TODO
+    return 0;
+}
+
+int calculate_dimes(int cents)
+{
+    // TODO
+    return 0;
+}
+
+int calculate_nickels(int cents)
+{
+    // TODO
+    return 0;
+}
+
+int calculate_pennies(int cents)
+{
+    // TODO
+    return 0;
 }
