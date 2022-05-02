b7bd07ce9449c09a2841f1278f512f15b3cdfcad
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-05-02 13:59:34 +0000

    Mon, May 2, 2022, 1:59 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -43,7 +43,7 @@ int count_letters(string text)
 //        }
 //        return x;
 //
-//int count_words()
+//int count_words(string text)
         //int x = 0;
 //        if(isalpha(text[x]))
 //        {
