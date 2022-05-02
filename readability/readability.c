1f8051611160fb31529e92257d647009d245c9bb
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-05-02 15:43:00 +0000

    Mon, May 2, 2022, 3:43 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -8,27 +8,24 @@ int count_letters(string text);
 
 int main(void)
 {
-    int letters;
-    
     string text = get_string("Text: ");
-    int a ;
-    for (a = 0 ; text[a] != 0 ; a++ )
-    {
-    letters = count_letters(text);
+    int letters = count_letters(text);
     //int words = count_words(letters);
-    }
     printf("Number %i \n" , letters);
 }
 
-
-int count_letters(string text)
+int a ;
+for (a = 0 ; text[a] != 0 ; a++ )
 {
+  int count_letters(string text)
+  {
         int l = 0;
         while(isalpha(text[l]))
         {
          l++;
         }
         return l;
+  }
 }
 
 //while (x != 0)
