7ca95114028d98beabae4865309837f0ff3dfe65
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-05-03 16:43:09 +0000

    Tue, May 3, 2022, 4:43 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -17,7 +17,7 @@ int main(void)
 
     float L = letters/words*100;
     float S = sentences/words*100;
-    int index = 0.0588 * L - 0.296 * S - 15.8, round(L), round(L);
+    int index = (0.0588 * %i - 0.296 * %i - 15.8,(int) round(L),(int) round(S));
     if(index >= 16)
     {
       printf("Grade 16+\n");
