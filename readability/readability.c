3cc3e03a8be90d33e59e10a0fe984c4214fad989
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-05-03 16:21:14 +0000

    Tue, May 3, 2022, 4:21 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -20,15 +20,15 @@ int main(void)
     float index = 0.0588 * L - 0.296 * S - 15.8;
     if(index >= 16)
     {
-      printf("Grade 16+");
+      printf("Grade 16+\n");
     }
     else if(index < 1)
     {
-      printf("Before Grade 1");
+      printf("Before Grade 1\n");
     }
     else
     {
-    printf("Grade %i" , index);
+    printf("Grade %i\n" ,float round(index));
     }
 }
 
