6599b4e9c0e57ba08c445b9f85c9e7f6931db059
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-05-03 14:49:27 +0000

    Tue, May 3, 2022, 2:49 PM +00:00

--- readability/readability.c
+++ readability/readability.c
@@ -11,7 +11,7 @@ int main(void)
 {
     string text = get_string("Text: ");
     int letters = count_letters(text);
-    int words = count_words(text);
+    int words = count_words(text) - ;
     printf("Number %i %i \n" , letters , words);
 }
 
@@ -51,5 +51,5 @@ int count_words(string text)
         l++;
         }
         int w;
-       return w = l - d - ;
+       return w = l - d -  ;
 }
