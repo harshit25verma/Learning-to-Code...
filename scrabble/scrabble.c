d63a58dc8060bc9ff76e98e11b5ee5db5b498623
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-08-02 15:01:14 +0000

    Tue, Aug 2, 2022, 3:01 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -8,10 +8,10 @@
 int POINTS[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
 
 int compute_score(string word);
-
+int s = 0;
 int main(void)
 {
-    int s = 0;
+
     // Get input words from both players
     string word = get_string("Player 1: ");
     //string word2 = get_string("Player 2: ");
@@ -46,7 +46,7 @@ int compute_score(string word)
       n++;
 
       }
-      int s = (word[w]) + s;
+      s = (word[w]) + s;
       w++;
       }
       return s;
