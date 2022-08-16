fdadb6356b8bf5da43f1ac5e0896067729bea3c4
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2022-08-16 18:41:50 +0000

    Tue, Aug 16, 2022, 6:41 PM +00:00

--- scrabble/scrabble.c
+++ scrabble/scrabble.c
@@ -14,11 +14,11 @@ int main(void)
 
     // Get input words from both players
     string word = get_string("Player 1: ");
-    //string word2 = get_string("Player 2: ");
+    string word2 = get_string("Player 2: ");
 
     // Score both words
     int score1 = compute_score(word);
-    //int score2 = compute_score(word2);
+    int score2 = compute_score(word2);
 
     // TODO: Print the winner
     printf ("Score = %f", s);
