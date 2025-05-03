84bcdfdec9038f904f04d066316ac7dae7fb4f74
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-03 22:16:38 +0000

    Sun, May 4, 2025, 12:16 AM +02:00

--- CS50x_2025/cash/cash.c
+++ CS50x_2025/cash/cash.c
@@ -14,24 +14,24 @@ int main(void)
     while(amount<0 || amount>100);
 
 //Divide the amount first with 25
-int 25 = amount/25;
+int a = amount/25;
 //Get the number of coins
 
 //Divide the amount second with 10
-int 10 = amount/10;
+int b = amount/10;
 //Get the number of coins
 
 //Divide the amount third with 5
-int 5 = amount/5;
+int c = amount/5;
 //Get the number of coins
 
 //Divide the amount fourht with 1
-int 1 = amount/1;
+int d = amount/1;
 //Get the number of coins
 
 //Add the the number of coins
-int sum = 25+10+5+1
+int sum = a+b+c+d ;
 //Show the number of coins
-printf("")
+printf("%i", int sum);
 
 }
