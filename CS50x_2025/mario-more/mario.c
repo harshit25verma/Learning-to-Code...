66843a78b34b2f9991354b890ef6075534ca8fbc
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-03 20:25:44 +0000

    Sat, May 3, 2025, 10:25 PM +02:00

--- CS50x_2025/mario-more/mario.c
+++ CS50x_2025/mario-more/mario.c
@@ -11,20 +11,38 @@ int main(void)
     while(height<1);
 
     int rows;
+    int spaces;
+    
     for (rows = 1; rows <= height; rows++)
     {
-        int spaces;
-        for (spaces = 0; spaces < height - rows; spaces++)
-        {
-            printf(" ");
-        }
         int bricks;
-        for(bricks = 0; bricks < rows; bricks++)
-        {
-            printf("#");
-        }
+
+        spaces(rows, height, rows);
+        bricks(rows)
+
         printf(" ");
-        
+
         printf("\n");
+    }
+}
+
+
+
+
+
+
+void spaces(int spaces, int height, int rows)
+{
+    for (spaces = 0; spaces < height - rows; spaces++)
+    {
+        printf(" ");
+    }
+}
+
+void bricks(int bricks, int rows)
+{
+    for(bricks = 0; bricks < rows; bricks++)
+    {
+        printf("#");
     }
 }
