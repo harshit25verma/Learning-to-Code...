c6f7a6fdfa90722da02ce9ae6db0d311136a4f80
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-03 19:12:25 +0000

    Sat, May 3, 2025, 9:12 PM +02:00

--- CS50x_2025/mario-more/mario.c
+++ CS50x_2025/mario-more/mario.c
@@ -5,8 +5,13 @@ int main(void)
 {
     do
     {
-        
+        int height = get_int("Height: ");
     }
+    while(height<1);
+
     int rows;
     for (int rows = 0; rows<height; rows++)
+    {
+        
+    }
 }
