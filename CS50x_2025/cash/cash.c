26d444ca5385fb70c9b57f447468da511334ed31
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-03 22:46:28 +0000

    Sun, May 4, 2025, 12:46 AM +02:00

--- CS50x_2025/cash/cash.c
+++ CS50x_2025/cash/cash.c
@@ -15,24 +15,19 @@ int main(void)
 int quarters = amount/25;
 //Multiply 25 with number of coins
 int remaining_Q = amount - (quarters*25);
-//Get the number of coins
-
 
 //Divide the amount second with 10
 int tens = remaining_Q/10;
 //Multiply  with number of coins
 int remaining_T = remaining_Q - (tens*10);
-//Get the number of coins
 
 //Divide the amount third with 5
 int fives = remaining_T/5;
 //Multiply  with number of coins
 int remaining_F = remaining_T - (fives*5);
-//Get the number of coins
 
 //Divide the amount fourht with 1
 int ones = remaining_F/1;
-//Get the number of coins
 
 //Add the the number of coins
 int sum = quarters+tens+fives+ones ;
