165c0faa8898dd6438ac5112ad307407876956aa
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-03 21:34:04 +0000

    Sat, May 3, 2025, 11:34 PM +02:00

--- CS50x_2025/cash/cash.c
+++ CS50x_2025/cash/cash.c
@@ -3,6 +3,11 @@
 
 int main(void)
 {
-    25 , 10 , 5 , 1
-    
+ //   25 , 10 , 5 , 1
+int amount;
+    do
+    {
+        amount = get_int("Change owed: ");
+    }
+    while(amount > 0 || amount < 100);
 }
