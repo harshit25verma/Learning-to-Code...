05af7bb2b7aaf58417876fc8d92cc676c3b4dc82
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-10 13:00:42 +0000

    Sat, May 10, 2025, 3:00 PM +02:00

--- CS50x_2025/scrabble/scrabble.c
+++ CS50x_2025/scrabble/scrabble.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 #include <cs50.h>
 
-int value[65 OR 65+32] = 1;
-int value[66] = 1;
-int value[67] = 1;
-int value[68] = 1;
-int value[69] = 1;
+int value[65] = 1;
+int value[66] = 3;
+int value[67] = 3;
+int value[68] = 2;
+int value[69] = ;
 int value[70] = 1;
 int value[71] = 1;
 int value[72] = 1;
@@ -17,19 +17,15 @@ int value[77] = 1;
 int value[78] = 1;
 int value[79] = 1;
 int value[80] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-int value[65] = 1;
-
-
+int value[81] = 1;
+int value[83] = 1;
+int value[84] = 1;
+int value[85] = 1;
+int value[86] = 1;
+int value[87] = 1;
+int value[88] = 1;
+int value[89] = 1;
+int value[90] = 1;
 
 int main (void)
 {
