907c81a1c6d65d9901f627752fe96ad61a2d1c88
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-10 12:53:11 +0000

    Sat, May 10, 2025, 2:53 PM +02:00

--- CS50x_2025/scrabble/scrabble.c
+++ CS50x_2025/scrabble/scrabble.c
@@ -10,15 +10,3 @@ int main (void)
     string player_two = get_string("Player 2: ");
 
 }
-
-void main ()
-{
-    player_one[n]
-}
-
-    int n = 0;
-    while (player_one[n] != '\0')
-    {
-        n++;
-    }
-    return n;
