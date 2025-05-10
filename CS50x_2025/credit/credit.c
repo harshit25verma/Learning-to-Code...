007f5a1d9d2ea0fc44d216669e3de695ca0d4f38
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-10 11:17:42 +0000

    Sat, May 10, 2025, 1:17 PM +02:00

---
+++ CS50x_2025/credit/credit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <cs50.h>
+
+int main(void)
+{
+
+    long Card_Number = get_long("Number: ");
+
+    first = Card_Number % 10;
+    
+    printf("VISA\n");
+    printf("AMEX\n");
+    printf("MASTERCARD\n");
+    printf("INVALID\n");
+
+
+
+
+}
