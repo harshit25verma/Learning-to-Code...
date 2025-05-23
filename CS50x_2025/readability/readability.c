3fe919645859158d756c3e454225a7db06781589
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-23 11:03:16 +0000

    Fri, May 23, 2025, 1:03 PM +02:00

---
+++ CS50x_2025/readability/readability.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <cs50.h>
+#include <ctype.h>
+
+int NumberOfWords();
+int NumberOfLetters();
+int NumberOfSentences();
+
+int main (void)
+{
+    while (word[n] = '\0')
+    {
+    string word = get_string("Text: ")     //get prompt from the User
+    }
+
+    //Check how many word and letter
+
+
+
+        //Check how many Letters are present in the first 100 words
+        //Count words until 100 words
+        if(word_count = 100)
+        {
+
+        }
+        else
+        {
+            break;
+        }
+        //Check How many Sentences are present in the first 100 words
+
+    //Calculate via the Coleman-Liau index
+
+    //Print the answer
+
+}
+
+
+//Check how many word
+int NumberOfWords()
+{
+    int word_count = 0;
+    while(isspace(word[n]))
+    {
+        word_count++;
+    }
+}
+
+//Check how many letters
+int NumberOfLetters()
+{
+    int letter_count = 0;
+    while(isalnum(word[n]))
+    {
+        letter_count++;
+    }
+}
+
+//Check how many sentences
+int NumberOfSentences()
+{
+    int sentence_count = 0;
+    while(ispunct(word[n]))
+    {
+        sentence_count++;
+    }
+}
--- CS50x_2025/scrabble/scrabble.c
+++ CS50x_2025/scrabble/scrabble.c
@@ -1,39 +1,104 @@
 #include <stdio.h>
 #include <cs50.h>
-
+#include <ctype.h>
+//              A, B, C, D, E, F, G, H, I, J, K, L, M, N, O, P, Q, R, S, T, U, V, W,  X, Y, Z
 int Points[] = {1, 3, 3, 2, 1, 4, 2, 4, 1, 8, 5, 1, 3, 1, 1, 3, 10, 1, 1, 1, 1, 4, 4, 8, 4, 10};
+int Scoreboard[] = {};
 
+long Convert(string word);
+void compare_score (int Score);
 
 int main (void)
 {
-    string word1 = get_string("Player 1: ");
-    string word2 = get_string("Player 2: ");
+    string word;
+    int Score;
+    int n = 0;
 
+      for(int player = 1; player <= 2; player++)
+      {
+        do
+        {
+        word = get_string("Player %i: ", player);
+        }
+        while (isblank(word[n]) || isdigit(word[n]) || isspace(word[n]));
 
-        //run a loop to go to the character of a string
-             int n = 0;
-                while (word1[n] != '\0')    //repeat until '\0'(null) found
-                {
-                    //check if the character ascii value is what
-                  printf("%i-65 ", word1[n]);
-                    //change char to an int value for scrabble rules
-                    alphabet = (%i - 65, word1[n]);
-                    //give this value back
-
-                    n++;    //go to the next character
-                }
-
-    //take the value from the above loop for player_one
-    //add them one by one
-
-    //take the value from the above loop for player_two
-    //add them one by one
-
-    //compare player_one and player_two
-    //if player_one > player_two, print Player 1 wins!
-    //if player_two < player_one, print Player 2 wins!
-    //if player_one = player_two, Tie!
+        Score = Convert(word);
+        Scoreboard[player] = Score;
+      }
+      compare_score(Score);
 }
+
+
+
+
+
+
+
+//compare player_one and player_two
+void compare_score (int Score)
+{
+  if (Scoreboard[1] > Scoreboard[2])
+  {
+    printf("Player 1 wins!\n");     //if player_one > player_two, print Player 1 wins!
+  }
+    else if (Scoreboard[1] < Scoreboard[2])
+    {
+          printf("Player 2 wins!\n");     //if player_two < player_one, print Player 2 wins!
+    }
+      else if (Scoreboard[1] == Scoreboard[2])
+      {
+            printf("Tie!\n");     //if player_one = player_two, Tie!
+      }
+
+}
+
+
+
+
+
+
+
+
+
+long Convert(string word)
+  {
+   //run a loop to go to the character of a string
+   int n = 0;
+   int Score = 0;
+   int Alphabet;
+   int UppAlphabet;
+   while (word[n] != '\0')    //repeat until '\0'(null) found
+   {
+   //If it is a alphabet
+    if (isalpha(word[n]))
+    {
+        //If Lowercase, then impment this function
+        if (islower(word[n]))
+        {
+            Alphabet = (word[n] - 97); //change char to an int value for scrabble rules
+        }
+        //Else if Uppercase, implement this function
+        else if (isupper(word[n]))
+        {
+            Alphabet = (word[n] - 65); //change char to an int value for scrabble rules
+        }
+    }
+    //else, break the og if/else loop and go out to implement next steps
+    else
+    {
+      break;
+    }
+
+      Score += Points[Alphabet]; //Score is equal to the Points of LowAlphabet value and added at each iteration(add them one by one)
+
+      n++;    //go to the next character
+
+   }
+
+     // printf("%i\n", Score);
+      return Score;
+
+  }
 
 
 
