8bb46c159775ed728ea9256a6ea1b16a43582cee
Harshit25Verma <103148748+Harshit25Verma@users.noreply.github.com>
2025-05-25 12:32:15 +0000

    Sun, May 25, 2025, 2:32 PM +02:00

--- CS50x_2025/readability/readability.c
+++ CS50x_2025/readability/readability.c
@@ -2,31 +2,38 @@
 #include <cs50.h>
 #include <ctype.h>
 
-int NumberOfWords();
-int NumberOfLetters();
-int NumberOfSentences();
+string User_Input();
+int NumberOfWords(string text_input);
+int NumberOfLetters(string text_input);
+int NumberOfSentences(string text_input);
+
+int n = 0;
 
 int main (void)
 {
-    while (word[n] = '\0')
+    string text;
+    text = User_Input();
+
+    while (text[n] != '\0')
     {
-    string word = get_string("Text: ")     //get prompt from the User
+    NumberOfWords(text);
+    NumberOfLetters(text);
+    NumberOfSentences(text);
+    n++;
     }
 
     //Check how many word and letter
-
-
 
         //Check how many Letters are present in the first 100 words
         //Count words until 100 words
         //if(word_count = 100)
         //{
 
-        }
-        else
-        {
-            break;
-        }
+        //}
+        //else
+        //{
+        //    break;
+        //}
         //Check How many Sentences are present in the first 100 words
 
     //Calculate via the Coleman-Liau index
@@ -35,33 +42,51 @@ int main (void)
 
 }
 
+//Get Text prompt from the User
+string User_Input()
+{
+    do
+    {
+        string text = get_string("Text: ");     //get prompt from the User
+        n++;
+    }
+    while (isblank(text_input[n]) || isdigit(text_input[n]) || isspace(text_input[n]));
+    return text;
+}
+
 
 //Check how many word
-int NumberOfWords()
+int NumberOfWords(string text)
 {
     int word_count = 0;
-    while(isspace(word[n]))
+    while(isspace(text[n]))
     {
         word_count++;
     }
+    printf("%i",word_count);
+    return word_count;
 }
 
 //Check how many letters
-int NumberOfLetters()
+int NumberOfLetters(string text)
 {
     int letter_count = 0;
-    while(isalnum(word[n]))
+    while(isalnum(text[n]))
     {
         letter_count++;
     }
+    printf("%i",letter_count);
+    return letter_count;
 }
 
 //Check how many sentences
-int NumberOfSentences()
+int NumberOfSentences(string text)
 {
     int sentence_count = 0;
-    while(ispunct(word[n]))
+    while(ispunct(text[n]))
     {
         sentence_count++;
     }
+    printf("%i",sentence_count);
+    return sentence_count;
 }
